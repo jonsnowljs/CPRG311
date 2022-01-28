50c0edb3230f0eea63d0ccaea791c35ef782bac0
Jiasheng Lu <1473875731@qq.com>
2022-01-28 00:51:00 -0700

    finish the cone and cylinder class

--- Assignment1/src/Shapes/Cone.java
+++ Assignment1/src/Shapes/Cone.java
@@ -1,25 +1,80 @@
 package Shapes;
 
+/**
+ * Inherit from shape class, represent the Cone shape
+ * @author Jiasheng Lu
+ *
+ */
 public class Cone extends Shape {
 	double radius;
 
+	/**
+	 * no-args constructor
+	 */
 	public Cone() {
-		// TODO Auto-generated constructor stub
-		//test another account
-		// add from new branch
+
+	}	
+
+	/**
+	 * Constructor with height and radius, create a cone object
+	 * @param radius
+	 */
+	public Cone(double radius, double height) {
+		this.radius = radius;
+		this.height = height;
+	}	
+
+	/**
+	 * get the height of cone
+	 * 
+	 * @return the height
+	 */
+	public double getHeight() {
+		return height;
 	}
 
-	// test
+	/**
+	 * set the height of cone
+	 * 
+	 * @param height the height to set
+	 */
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	/**
+	 * Get the radius of cone
+	 * 
+	 * @return the radius
+	 */
+	public double getRadius() {
+		return radius;
+	}
+
+	/**
+	 * Set the radius of cone
+	 * 
+	 * @param radius the radius to set
+	 */
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double calcVolume() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1/3 * Math.PI * Math.pow(radius, 2) * height;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double calcBaseArea() {
 		// TODO Auto-generated method stub
-		return 0;
+		return Math.PI * Math.pow(radius, 2);
 	}
 
 }
--- Assignment1/src/Shapes/Cylinder.java
+++ Assignment1/src/Shapes/Cylinder.java
@@ -1,22 +1,86 @@
 package Shapes;
 
+/**
+ * Inherit from shape class, represent the Cylinder shape
+ * @author Jiasheng Lu
+ *
+ */
 public class Cylinder extends Shape {
 	double radius;
 
+	/**
+	 * no-args constructor
+	 */
 	public Cylinder() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/**
+	 * Constructor with height and radius
+	 * 
+	 * @param height
+	 * @param radius
+	 */
+	public Cylinder(double height, double radius) {
+		this.height = height;
+		this.radius = radius;
+	}
 
+	/**
+	 * get the height of cylinder
+	 * 
+	 * @return the height
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/**
+	 * set the height of cylinder
+	 * 
+	 * @param height the height to set
+	 */
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+
+	/**
+	 * Get the radius of cylinder
+	 * 
+	 * @return the radius
+	 */
+	public double getRadius() {
+		return radius;
+	}
+
+
+	/**
+	 * Set the radius of cylinder
+	 * 
+	 * @param radius the radius to set
+	 */
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double calcVolume() {
 		// TODO Auto-generated method stub
-		return 0;
+		return Math.PI * Math.pow(radius, 2) * height;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double calcBaseArea() {
 		// TODO Auto-generated method stub
-		return 0;
+		return Math.PI * Math.pow(radius, 2);
 	}
 
 }
