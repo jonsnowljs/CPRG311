6e4d6655cf7e027cba2013be64993661cb7505a1
Jiasheng Lu <1473875731@qq.com>
2022-02-04 10:31:51 -0700

    test

--- Assignment1/src/sorting/AllSorts.java
+++ Assignment1/src/sorting/AllSorts.java
@@ -22,7 +22,7 @@ public class AllSorts {
 	}
 
 	public static <T extends Comparable<? super T>> void selectionSort(T[] arr) {
-
+		//	 test
 	}
 
 	public static <T extends Comparable<? super T>> void insertionSort(T[] arr) {
