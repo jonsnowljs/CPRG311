0ce9d2f59b613a3b8dba11a6672b981c44731bd4
Jiasheng Lu <1473875731@qq.com>
2022-02-03 17:33:16 -0700

    bugs in Allsorts

--- Assignment1/src/sorting/AllSorts.java
+++ Assignment1/src/sorting/AllSorts.java
@@ -1,17 +1,34 @@
 package sorting;
 
+import compareInterface.BaseAreaComp;
+import shapes.Shape;
+
 public class AllSorts {	
-	public static <T extends Comparable<? super T>> void bubbleSort(T[] arr) {
+	public static void bubbleSort(Shape[] arr, String command) {
+		
 	 int N = arr.length;
 	  for (int i = N - 1; i > 0; --i) { // i >= 0 --> OK
 	    for (int j = 0; j < i; ++j) {
 	      if (arr[j].compareTo(arr[j + 1]) > 0) {
-              T temp = arr[j];
+              Shape temp = arr[j];
               arr[j] = arr[j+1];
               arr[j+1] = temp;
 	      }
 	    }
 	  }
+	  
+
+	  for (int i = N - 1; i > 0; --i) { // i >= 0 --> OK
+	    for (int j = 0; j < i; ++j) {
+	      if (BaseAreaComp.compare(arr[j], arr[j+1]) > 0) {
+              Shape temp = arr[j];
+              arr[j] = arr[j+1];
+              arr[j+1] = temp;
+	      }
+	    }
+	  }
+	  
+	  
 	}
 	
 	public static <T extends Comparable<? super T>> void selectionSort(T[] arr) {
