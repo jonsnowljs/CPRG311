adcb96b647d2375978d2442ccac1a69c94a2e8e3
hardishc <contacthardish@gmail.com>
2022-02-06 14:40:53 -0700

    MergeSort

--- Assignment1/src/sorting/AllSorts.java
+++ Assignment1/src/sorting/AllSorts.java
@@ -13,7 +13,7 @@ public class AllSorts {
 		case 'H':
 			for (int i = N - 1; i > 0; --i) { // i >= 0 --> OK
 				for (int j = 0; j < i; ++j) {
-					if (arr[j].compareTo(arr[j+1]) > 0) {
+					if (arr[j].compareTo(arr[j + 1]) > 0) {
 						T temp = arr[j];
 						arr[j] = arr[j + 1];
 						arr[j + 1] = temp;
@@ -34,7 +34,7 @@ public class AllSorts {
 				}
 			}
 			break;
-			
+
 		case 'V':
 			for (int i = N - 1; i > 0; --i) { // i >= 0 --> OK
 				for (int j = 0; j < i; ++j) {
@@ -47,28 +47,81 @@ public class AllSorts {
 				}
 			}
 			break;
+
+		default:
+			System.out.println("Invalid Input");
+			System.out.println(
+					"Please use -TH for height comparison, -TA for base area comparison and -TV for volume comparison");
+		}
+
+	}
+
+//	Desmond
+	public static <T extends Comparable<? super T>> void selectionSort(T[] arr) {
+		// test
+	}
+
+	public static <T extends Comparable<? super T>> void insertionSort(T[] arr) {
+
+	}
+
+//Hardish
+	public static <T extends Comparable<? super T>> void mergeSort(T[] arr, char command) {
+		int N = arr.length;
+		
+		int mid = N / 2;
+	    T[] l = new T[mid];
+	    T[] r = new T[N - mid];
+
+		switch (Character.toUpperCase(command)) {
+		case 'H':
+			for (int i = 0; i < mid; i++) {
+		        l[i] = arr[i];
+		    }
+		    for (int i = mid; i < N; i++) {
+		        r[i - mid] = arr[i];
+		    }
+		    mergeSort(l, mid);
+		    mergeSort(r, N - mid);
+
+		    merge(arr, l, r, mid, N - mid);
+		
+			break;
+
+		case 'A':
+			
+			break;
+			
+		case 'V':
+			
+			break;
 		
 		default:
 			System.out.println("Invalid Input");
 			System.out.println("Please use -TH for height comparison, -TA for base area comparison and -TV for volume comparison");
 		}
 		
-		
-			
 	}
-
-//	Desmond
-	public static <T extends Comparable<? super T>> void selectionSort(T[] arr) {
-		//	 test
-	}
-
-	public static <T extends Comparable<? super T>> void insertionSort(T[] arr) {
-
-	}
-//Hardish
-	public static <T extends Comparable<? super T>> void mergeSort(T[] arr) {
-
-	}
+	
+	public static void merge(
+			  int[] arr, int[] l, int[] r, int left, int right) {
+			 
+			    int i = 0, j = 0, k = 0;
+			    while (i < left && j < right) {
+			        if (l[i] <= r[j]) {
+			            arr[k++] = l[i++];
+			        }
+			        else {
+			            arr[k++] = r[j++];
+			        }
+			    }
+			    while (i < left) {
+			        arr[k++] = l[i++];
+			    }
+			    while (j < right) {
+			        arr[k++] = r[j++];
+			    }
+			}
 
 	public static <T extends Comparable<? super T>> void quickSort(T[] arr) {
 
@@ -78,5 +131,5 @@ public class AllSorts {
 	public static <T extends Comparable<? super T>> void Sort(T[] arr) {
 
 	}
-	
+
 }
