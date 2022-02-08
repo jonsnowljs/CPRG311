fa5340f2c9e74e3794fde9764ce9b8d33db7235f
hardishc <contacthardish@gmail.com>
2022-02-07 21:30:37 -0700

    Implemented MergeSort

--- Assignment1/src/sorting/AllSorts.java
+++ Assignment1/src/sorting/AllSorts.java
@@ -9,6 +9,7 @@ import shapes.Shape;
 public class AllSorts {
 	/**
 	 * bubble sort for comparable array
+	 * 
 	 * @param <T>
 	 * @param arr
 	 */
@@ -17,18 +18,19 @@ public class AllSorts {
 
 		for (int i = N - 1; i > 0; --i) {
 			for (int j = 0; j < i; ++j) {
-				if (arr[j].compareTo(arr[j+1]) > 0) {
+				if (arr[j].compareTo(arr[j + 1]) > 0) {
 					T temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
 				}
 			}
 		}
-		
+
 	}
-	
+
 	/**
-	 * bubble sort for comparable array and comparator 
+	 * bubble sort for comparable array and comparator
+	 * 
 	 * @param <T>
 	 * @param arr
 	 * @param comparator
@@ -38,14 +40,14 @@ public class AllSorts {
 
 		for (int i = N - 1; i > 0; --i) {
 			for (int j = 0; j < i; ++j) {
-				if (comparator.compare(arr[j], arr[j+1]) > 0) {
+				if (comparator.compare(arr[j], arr[j + 1]) > 0) {
 					T temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
 				}
 			}
 		}
-		
+
 	}
 
 //	Desmond
@@ -56,64 +58,141 @@ public class AllSorts {
 	public static <T extends Comparable<? super T>> void insertionSort(T[] ar, Comparator<T> comparatorr) {
 
 	}
-
-//Hardish
-//	public static <T extends Comparable<? super T>> void mergeSort(T[] arr, Comparator<T> comparator) {
-//		int N = arr.length;
-//		
-//		int mid = N / 2;
-//	    T[] l = (T[])new Object[mid];
-//	    T[] r = (T[])new Object[N - mid];
-//
-//		switch (Character.toUpperCase(command)) {
-//		case 'H':
-//			for (int i = 0; i < mid; i++) {
-//		        l[i] = arr[i];
-//		    }
-//		    for (int i = mid; i < N; i++) {
-//		        r[i - mid] = arr[i];
-//		    }
-//		    mergeSort(l, mid);
-//		    mergeSort(r, N - mid);
-//
-//		    merge(arr, l, r, mid, N - mid);
-//		
-//			break;
-//
-//		case 'A':
-//			
-//			break;
-//			
-//		case 'V':
-//			
-//			break;
-//		
-//		default:
-//			System.out.println("Invalid Input");
-//			System.out.println("Please use -TH for height comparison, -TA for base area comparison and -TV for volume comparison");
-//		}
-//		
-//	}
+/**
+ * merge sorting using comparable
+ * 
+ * @param <T>
+ * @param arr
+ */
+	public static <T extends Comparable<? super T>> void mergeSort(T[] arr) {
+		if(arr == null)
+        {
+            return;
+        }
+ 
+        if(arr.length > 1)
+        {
+            int mid = arr.length / 2;
+ 
+            // Split left part
+            T[] left = (T[]) new Shape[mid];
+            for(int i = 0; i < mid; i++)
+            {
+                left[i] = arr[i];
+            }
+             
+            // Split right part
+            T[] right = (T[])new Shape[arr.length - mid];
+            for(int i = mid; i < arr.length; i++)
+            {
+                right[i - mid] = arr[i];
+            }
+            mergeSort(left);
+            mergeSort(right);
+ 
+            int i = 0;
+            int j = 0;
+            int k = 0;
+ 
+            // Merge left and right arrays
+            while(i < left.length && j < right.length)
+            {
+                if(left[i].compareTo(right[j]) < 0)
+                {
+                	arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                	arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+            // Collect remaining elements
+            while(i < left.length)
+            {
+            	arr[k] = left[i];
+                i++;
+                k++;
+            }
+            while(j < right.length)
+            {
+            	arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+    }
 	
-	public static void merge(
-			  int[] arr, int[] l, int[] r, int left, int right) {
-			 
-			    int i = 0, j = 0, k = 0;
-			    while (i < left && j < right) {
-			        if (l[i] <= r[j]) {
-			            arr[k++] = l[i++];
-			        }
-			        else {
-			            arr[k++] = r[j++];
-			        }
-			    }
-			    while (i < left) {
-			        arr[k++] = l[i++];
-			    }
-			    while (j < right) {
-			        arr[k++] = r[j++];
-			    }
-			}
+	/**
+	 * merge sorting using comparator
+	 * 
+	 * @param <T>
+	 * @param arr
+	 * @param comparator
+	 */
+	public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+		if(arr == null)
+        {
+            return;
+        }
+ 
+        if(arr.length > 1)
+        {
+            int mid = arr.length / 2;
+ 
+            // Split left part
+            T[] left = (T[]) new Shape[mid];
+            for(int i = 0; i < mid; i++)
+            {
+                left[i] = arr[i];
+            }
+             
+            // Split right part
+            T[] right = (T[])new Shape[arr.length - mid];
+            for(int i = mid; i < arr.length; i++)
+            {
+                right[i - mid] = arr[i];
+            }
+            mergeSort(left, comparator);
+            mergeSort(right, comparator);
+ 
+            int i = 0;
+            int j = 0;
+            int k = 0;
+ 
+            // Merge left and right arrays
+            while(i < left.length && j < right.length)
+            {
+                if(comparator.compare(left[i], right[j]) < 0)
+                {
+                	arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                	arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+            // Collect remaining elements
+            while(i < left.length)
+            {
+            	arr[k] = left[i];
+                i++;
+                k++;
+            }
+            while(j < right.length)
+            {
+            	arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+    }
+
 
 	public static <T extends Comparable<? super T>> void quickSort(T[] arr, Comparator<T> comparator) {
 
@@ -121,7 +200,7 @@ public class AllSorts {
 
 //Jiasheng
 	public static <T extends Comparable<? super T>> void Sort(T[] arr, Comparator<T> comparator) {
-		
+
 	}
 
 }
