dde34d2f49075c4637818fbb428fa937c55ea525
Jiasheng Lu <1473875731@qq.com>
2022-02-07 18:27:50 -0700

    update bubble sort with only comparable

--- Assignment1/src/sorting/AllSorts.java
+++ Assignment1/src/sorting/AllSorts.java
@@ -7,6 +7,32 @@ import compareInterface.VolumeComp;
 import shapes.Shape;
 
 public class AllSorts {
+	/**
+	 * bubble sort for comparable array
+	 * @param <T>
+	 * @param arr
+	 */
+	public static <T extends Comparable<? super T>> void bubbleSort(T[] arr) {
+		int N = arr.length;
+
+		for (int i = N - 1; i > 0; --i) {
+			for (int j = 0; j < i; ++j) {
+				if (arr[j].compareTo(arr[j+1]) > 0) {
+					T temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		
+	}
+	
+	/**
+	 * bubble sort for comparable array and comparator 
+	 * @param <T>
+	 * @param arr
+	 * @param comparator
+	 */
 	public static <T extends Comparable<? super T>> void bubbleSort(T[] arr, Comparator<T> comparator) {
 		int N = arr.length;
 
@@ -32,42 +58,42 @@ public class AllSorts {
 	}
 
 //Hardish
-	public static <T extends Comparable<? super T>> void mergeSort(T[] arr, Comparator<T> comparator) {
-		int N = arr.length;
-		
-		int mid = N / 2;
-	    T[] l = (T[])new Object[mid];
-	    T[] r = (T[])new Object[N - mid];
-
-		switch (Character.toUpperCase(command)) {
-		case 'H':
-			for (int i = 0; i < mid; i++) {
-		        l[i] = arr[i];
-		    }
-		    for (int i = mid; i < N; i++) {
-		        r[i - mid] = arr[i];
-		    }
-		    mergeSort(l, mid);
-		    mergeSort(r, N - mid);
-
-		    merge(arr, l, r, mid, N - mid);
-		
-			break;
-
-		case 'A':
-			
-			break;
-			
-		case 'V':
-			
-			break;
-		
-		default:
-			System.out.println("Invalid Input");
-			System.out.println("Please use -TH for height comparison, -TA for base area comparison and -TV for volume comparison");
-		}
-		
-	}
+//	public static <T extends Comparable<? super T>> void mergeSort(T[] arr, Comparator<T> comparator) {
+//		int N = arr.length;
+//		
+//		int mid = N / 2;
+//	    T[] l = (T[])new Object[mid];
+//	    T[] r = (T[])new Object[N - mid];
+//
+//		switch (Character.toUpperCase(command)) {
+//		case 'H':
+//			for (int i = 0; i < mid; i++) {
+//		        l[i] = arr[i];
+//		    }
+//		    for (int i = mid; i < N; i++) {
+//		        r[i - mid] = arr[i];
+//		    }
+//		    mergeSort(l, mid);
+//		    mergeSort(r, N - mid);
+//
+//		    merge(arr, l, r, mid, N - mid);
+//		
+//			break;
+//
+//		case 'A':
+//			
+//			break;
+//			
+//		case 'V':
+//			
+//			break;
+//		
+//		default:
+//			System.out.println("Invalid Input");
+//			System.out.println("Please use -TH for height comparison, -TA for base area comparison and -TV for volume comparison");
+//		}
+//		
+//	}
 	
 	public static void merge(
 			  int[] arr, int[] l, int[] r, int left, int right) {
