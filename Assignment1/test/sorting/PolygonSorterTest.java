2747f199dfbcbf5abeba3ba5051940fd676ef649
Jiasheng Lu <1473875731@qq.com>
2022-02-13 19:19:50 -0700

    Update PolygonSorterTest.java

--- Assignment1/test/sorting/PolygonSorterTest.java
+++ Assignment1/test/sorting/PolygonSorterTest.java
@@ -5,10 +5,6 @@ package sorting;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import compare_interface.BaseAreaComp;
@@ -25,34 +21,6 @@ public class PolygonSorterTest {
 	String[] args2 = { "-fpolyfortest.txt", "-tv", "-sz" };
 	String[] args3 = { "-FPOlyfortest.txt", "-Ta", "-SI" };
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	/**
 	 * Test method for
