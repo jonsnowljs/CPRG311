55a5ec2dd83eb20be3a1e3403ec917eab0858be9
Desmond Yuen <90860427+DesmondYuen1@users.noreply.github.com>
2022-03-24 21:21:37 -0600

    MyArrayList+MyArrayListTest Completed.

--- Assignment2/src/utilities/MyArrayList.java
+++ Assignment2/src/utilities/MyArrayList.java
@@ -3,124 +3,248 @@ package utilities;
 import java.util.NoSuchElementException;
 
 @SuppressWarnings("unchecked")
-public class MyArrayList <E> implements ListADT<E>, Iterator<E>{
-	
-	//private static final long serialVersionUID = 4344482990L;
-	private static final int DEFAULT_SIZE = 5; //default size
-	private static final double DEFAULT_RATE = 0.75; //load factor
-	
-	private E[] array;
+public class MyArrayList<E> implements ListADT<E>, Iterator<E> {
+
+	// private static final long serialVersionUID = 4344482990L;
+	private static final int DEFAULT_SIZE = 5; // default size
+	private static final double DEFAULT_RATE = 0.75; // load factor
+
+	private E[] a;
+	private int current;
 	private int length;
 	private int size;
 	private double rate;
-	
-	//constructor to set the size of array
+
+	// constructor to set the size of array
 	public MyArrayList() {
-		this.size = DEFAULT_SIZE;
+		this(DEFAULT_SIZE);
+	}
+
+	public MyArrayList(int capacity) {
+		size = 0;
+		current = 0;
+		a = (E[]) new Object[capacity];
 	}
 
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+		return current < size && a[current] != null;
 	}
 
 	@Override
 	public E next() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (a[current + 1] != null) {
+			return a[current++];
+		} else
+			throw new NoSuchElementException();
+
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		size = 0;
+		current = 0;
+		a = (E[]) new Object[DEFAULT_SIZE];
 	}
 
 	@Override
 	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return false;
+		if (toAdd == null) {
+			throw new NullPointerException();
+		} else if (index < 0 || index > this.size || index > a.length) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (this.size == a.length) {
+			grow();
+		}
+		if (index == this.size) {
+			return add(toAdd);
+		} else {
+			for (int i = this.size - 1; i >= index; i--) {
+				a[i + 1] = a[i];
+			}
+			a[index] = toAdd;
+			this.size++;
+			return true;
+		}
+
 	}
 
 	@Override
 	public boolean add(E toAdd) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return false;
+		if (toAdd == null) {
+			throw new NullPointerException();
+		}
+		if (this.size == a.length) {
+			grow();
+		}
+		a[this.size] = toAdd;
+		this.size++;
+		return true;
 	}
 
 	@Override
 	public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return false;
+		if (toAdd == null) {
+			throw new NullPointerException();
+		}
+		for (int i = 0; i < toAdd.size(); i++) {
+			if (toAdd.get(i) != null) {
+				if (this.size == a.length) {
+					grow();
+				}
+				a[size] = (toAdd.get(i));
+				size++;
+			}
+		}
+		return true;
+
 	}
 
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		if (index >= this.size || index < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		E e = (E) a[index];
+		return e;
 	}
 
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		if (index >= this.size || index < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		E returnElement = a[index];
+		for (int i = index + 1; i < this.size; i++) {
+			a[i - 1] = a[i];
+		}
+		a[this.size] = null;
+		this.size--;
+		return returnElement;
 	}
 
 	@Override
 	public E remove(E toRemove) throws NullPointerException {
-		// TODO Auto-generated method stub
+		if (toRemove == null) {
+			throw new NullPointerException();
+		}
+		for (int i = 0; i < this.size; i++) {
+			if (a[i] == toRemove || a[i].equals(toRemove)) {
+				E returnElement = a[i];
+				for (int j = i + 1; j < this.size; j++) {
+					a[j - 1] = a[j];
+				}
+				a[this.size] = null;
+				this.size--;
+				return returnElement;
+			}
+		}
+
 		return null;
 	}
 
 	@Override
 	public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		if (toChange == null) {
+			throw new NullPointerException();
+		} else if (index < 0 || index >= this.size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (index >= a.length) {
+			for (int i = a.length; i <= index + 1; i++) {
+				grow();
+			}
+		}
+		E returnElement = a[index];
+		a[index] = toChange;
+		if (returnElement == null) {
+			size++;
+		}
+		return returnElement;
+
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		for (int i = 0; i < size; i++) {
+			if (a[i] != null)
+				return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean contains(E toFind) throws NullPointerException {
-		// TODO Auto-generated method stub
+		if (toFind == null) {
+			throw new NullPointerException();
+		}
+		for (int i = 0; i < this.size; i++) {
+			if (a[i] == toFind || a[i].equals(toFind)) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public E[] toArray(E[] toHold) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return null;
+		if (toHold == null) {
+			throw new NullPointerException();
+		}
+		if (toHold.length < this.size) {
+			E[] tempArray = (E[]) new Object[this.size];
+			for (int i = 0; i < this.size; i++) {
+				tempArray[i] = a[i];
+			}
+			return tempArray;
+		} else {
+			for (int i = 0; i < this.size; i++) {
+				toHold[i] = a[i];
+			}
+			return toHold;
+		}
+
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		E[] tempArray = (E[]) new Object[this.size];
+		for (int i = 0; i < this.size; i++) {
+			tempArray[i] = a[i];
+		}
+		return tempArray;
 	}
 
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+
+		Iterator<E> it = new Iterator<E>() {
+			@Override
+			public boolean hasNext() {
+				return current < size && a[current] != null;
+			}
+
+			@Override
+			public E next() {
+				return a[current++];
+			}
+		};
+		return it;
 	}
 
-	
-	/*
-	 * Returns the size of the array 
-	 */
-
-
-	
+	private void grow() {
+		int arrayLength = a.length;
+		E[] tempArray = (E[]) new Object[arrayLength + 1];
+		for (int i = 0; i < this.size; i++) {
+			tempArray[i] = a[i];
+		}
+		a = tempArray;
+	}
 
 }
--- Assignment2/test/utilities/MyArrayListTest.java
+++ Assignment2/test/utilities/MyArrayListTest.java
@@ -9,14 +9,14 @@ import org.junit.Test;
 import utilities.MyArrayList;
 
 public class MyArrayListTest {
-	
-	//Attributes
-		MyArrayList<Integer> mylist = new MyArrayList();
-		private Integer one;
-		private Integer two;
-		private Integer three;
-		private Integer four;
-		private Integer five;
+
+	// Attributes
+	MyArrayList<Integer> mylist = new MyArrayList();
+	private Integer one;
+	private Integer two;
+	private Integer three;
+	private Integer four;
+	private Integer five;
 
 	@Before
 	public void setUp() throws Exception {
@@ -25,9 +25,8 @@ public class MyArrayListTest {
 		three = new Integer(13);
 		four = new Integer(14);
 		five = new Integer(15);
-		
+
 	}
-	
 
 	@After
 	public void tearDown() throws Exception {
@@ -41,16 +40,185 @@ public class MyArrayListTest {
 
 	@Test
 	public void testSize() {
-		int expected = 5;
 		mylist.add(11);
 		mylist.add(12);
 		mylist.add(13);
 		mylist.add(14);
 		mylist.add(15);
 		int actual = mylist.size();
-		assertEquals(expected,actual);
+		assertEquals(5, actual);
+
+	}
+
+	@Test
+	public void testGet() {
+		MyArrayList<String> list = new MyArrayList<String>();
+		list.add(0, "test");
+		String actual = list.get(0);
+		assertEquals("test", actual);
+
+	}
+
+	@Test
+	public void testAdd() {
+		MyArrayList<String> list = new MyArrayList<String>();
+		boolean actual = list.add("test");
+		list.add("test");
+		list.add("test");
+		list.add("test");
+		list.add("test");
+		list.add("test");
+		list.add("test");
+		list.add("test");
+		list.add("test");
+		assertEquals(true, actual);
+
+	}
+
+	@Test
+	public void testAddWithIndex() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(0, 0);
+		int actual = list.get(0);
+		assertEquals(0, actual);
+
+	}
+
+	@Test
+	public void testAddAll() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		MyArrayList<Integer> list2 = new MyArrayList<Integer>();
+		list2.add(4);
+		list2.add(5);
+		list2.add(6);
+		boolean actual = list.addAll(list2);
+		assertEquals(true, actual);
+
+	}
+
+	@Test
+	public void testGrow() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(1);
+		list.add(1);
+		list.add(1);
+		list.add(1);
+		list.add(1);
+		int actual = list.size();
+		assertEquals(6, actual);
+
+	}
+	
+	@Test
+	public void testClear() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(1);
+		list.add(1);
+		list.add(1);
+		list.clear();
+		int actual = list.size();
+		assertEquals(0, actual);
+
+	}
+	
+	@Test
+	public void testRemove() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.remove(0);
+		int actual = list.get(0);
+		assertEquals(2, actual);
+
+	}
+	
+	@Test
+	public void testRemoveByElement() {
+		MyArrayList<String> list = new MyArrayList<String>();
+		list.add("test");
+		list.add("test2");
+		list.remove("test");
+		assertEquals("test2", list.get(0));
+
+	}
+	
+	@Test
+	public void testSet() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.set(0, 5);
+		int actual = list.get(0);
+		assertEquals(5, actual);
+
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		boolean actual = list.isEmpty();
+		assertEquals(false, actual);
+
+	}
+	
+	@Test
+	public void testContains() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		assertEquals(true, list.contains(3));
+
+	}
+	
+	@Test
+	public void testToArrayByToHold() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		Integer[] toHold = new Integer[2];
+		Object[] tempArray = list.toArray(toHold);
+		int actual = (int) tempArray[0];
+		assertEquals(1, actual);
+
+	}
+	
+	@Test
+	public void testToArray() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		Object[] tempArray = list.toArray();
+		int actual = (int) tempArray[0];
+		assertEquals(1, actual);
+
+	}
+	
+	@Test
+	public void testHasNextAndNext() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		assertEquals(true, list.hasNext());
+		int actual = list.next();
+		assertEquals(2, actual);
 		
-		
+
 	}
 
 }
