f2c420ff12b28c9291a6c952f3ade5cf5e61a5c0
Jiasheng Lu <1473875731@qq.com>
2022-04-23 17:49:38 -0600

    feat: implement bstree

--- Assignment3/src/implement/BSTree.java
+++ Assignment3/src/implement/BSTree.java
@@ -3,6 +3,8 @@
  */
 package implement;
 
+import java.util.NoSuchElementException;
+
 import utilities.BSTreeADT;
 import utilities.BSTreeNode;
 import utilities.Iterator;
@@ -15,39 +17,72 @@ import utilities.TreeException;
  */
 public class BSTree<E extends Comparable<? super E>> implements BSTreeADT<E> {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2202313053604159123L;
+	
+	BSTreeNode<E> root = null;
+	int size = 0;
+	
+	public BSTree(BSTreeNode<E> root) {
+		this.root = root;
+		this.size = 0;
+	}
+
 	@Override
-	public BSTreeNode<E> getRoot() throws TreeException {
-		// TODO Auto-generated method stub
-		return null;
+	public BSTreeNode<E> getRoot() {
+		return this.root;
 	}
 
 	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (root == null) {
+			return -1;
+		}
+		return getHeight(root);
 	}
 
+	private int getHeight(BSTreeNode<E> node) {
+		if (node == null) {
+			return 0;
+		} else {
+			int lHeight = getHeight(node.getLeft());
+			int rHeight = getHeight(node.getRight());
+
+			if (lHeight > rHeight) {
+				return (lHeight + 1);
+			} else {
+				return (rHeight + 1);
+			}
+		}
+	}
+	
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return size == 0;
 	}
-
+ 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		this.root = null;
+		this.size = 0;	
 	}
 
 	@Override
 	public boolean contains(E entry) throws TreeException {
-		// TODO Auto-generated method stub
-		return false;
+		if (root == null) {
+			return false;
+		} else {
+			return contains(entry);
+		}
+		
 	}
 
 	@Override
@@ -58,14 +93,36 @@ public class BSTree<E extends Comparable<? super E>> implements BSTreeADT<E> {
 
 	@Override
 	public boolean add(E newEntry) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return false;
+		BSTreeNode<E> newNode = new BSTreeNode<E>(newEntry, null, null);
+		
+		if (root == null) {
+			root = newNode;
+			return true;
+		} else {
+			return root.add(newNode);
+			
+		}
+
 	}
 
 	@Override
 	public Iterator<E> inorderIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		Iterator<E> it = new Iterator<E>() {
+			@Override
+			public boolean hasNext() {
+				return 
+			}
+
+			@Override
+			public E next() {
+				if (!this.hasNext()) {
+					throw new NoSuchElementException();
+				}
+				return root.
+			}
+		};
+		return it;
+		
 	}
 
 	@Override
@@ -79,6 +136,7 @@ public class BSTree<E extends Comparable<? super E>> implements BSTreeADT<E> {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 
 	
 
--- Assignment3/src/utilities/BSTreeNode.java
+++ Assignment3/src/utilities/BSTreeNode.java
@@ -1,6 +1,6 @@
 package utilities;
 
-public class BSTreeNode<E> {
+public class BSTreeNode<E extends Comparable<? super E>> {
 	E data;
 	BSTreeNode<E> left;
 	BSTreeNode<E> right;
@@ -10,4 +10,103 @@ public class BSTreeNode<E> {
 		this.left = null;
 		this.right = null;
 	}
+	
+	public BSTreeNode(E data, BSTreeNode<E> left, BSTreeNode<E> right) {
+		this.data = data;
+		this.left = left;
+		this.right = right;
+	}
+	
+	public boolean add(BSTreeNode<E> node) {
+		if (node == null) {
+			return false;
+		}
+		
+		if (node.data.compareTo(this.data) < 0) {
+			if (this.left == null) {
+				this.left = node;
+			} else {
+				this.left.add(node);
+			}
+		} else {
+			if (this.right == null) {
+				this.right = node;
+			} else {
+				this.right.add(node);
+			}
+		}
+		return true;
+	}
+	
+	public boolean contains(BSTreeNode<E> node) {
+		if (node == null) {
+			return false;
+		}
+		
+		if (node.data.compareTo(this.data) < 0) {
+			this.contains(left);
+		} else if (node.data.compareTo(this.data) > 0) {
+			this.contains(right);
+		} else {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void inorder( ) {
+		if (this.left != null) {
+			this.left.inorder();
+		}
+		
+		if (this.right != null) {
+			this.right.inorder();
+		}
+	}
+	
+ 
+
+
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(E data) {
+		this.data = data;
+	}
+
+	/**
+	 * @return the left
+	 */
+	public BSTreeNode<E> getLeft() {
+		return left;
+	}
+
+	/**
+	 * @param left the left to set
+	 */
+	public void setLeft(BSTreeNode<E> left) {
+		this.left = left;
+	}
+
+	/**
+	 * @return the right
+	 */
+	public BSTreeNode<E> getRight() {
+		return right;
+	}
+
+	/**
+	 * @param right the right to set
+	 */
+	public void setRight(BSTreeNode<E> right) {
+		this.right = right;
+	}
+
+	public E getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 }
